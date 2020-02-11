199b10395d0d3e387c766a71124aa14785281c81
ml <30815731+slimlime@users.noreply.github.com>
2020-02-11 17:52:46 +1000

    Deserialisation without parameterless constructor error

--- Models/ToDoItem.cs
+++ Models/ToDoItem.cs
@@ -3,11 +3,5 @@ namespace ToDoApi.Models {
         public long ID { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
-
-        public ToDoItem(long ID, string name, bool isComplete) {
-            this.ID = ID;
-            this.Name = name;
-            this.IsComplete = isComplete;
-        }
     }
 }
