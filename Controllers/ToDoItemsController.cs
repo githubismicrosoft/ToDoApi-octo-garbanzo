0b92b7fdb058523f1db58d8c28d15a6ecf119d4b
ml <30815731+slimlime@users.noreply.github.com>
2020-02-11 20:15:55 +1000

    :art: trailing brace vs new line formatting. K&R C vs Microsoft .NET default convention.  Saves line verticality SLOC though affects comprehension/readability on sufficiently high-resolution screens.

--- Controllers/ToDoItemsController.cs
+++ Controllers/ToDoItemsController.cs
@@ -7,64 +7,51 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ToDoApi.Models;
 
-namespace ToDoApi.Controllers
-{
+namespace ToDoApi.Controllers {
     [Route("api/[controller]")]
     [ApiController]
-    public class ToDoItemsController : ControllerBase
-    {
+    public class ToDoItemsController : ControllerBase {
         private readonly ToDoContext _context;
 
-        public ToDoItemsController(ToDoContext context)
-        {
+        public ToDoItemsController(ToDoContext context) {
             _context = context;
         }
 
         // GET: api/ToDoItems
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<ToDoItem>>> GetToDoItems()
-        {
+        public async Task<ActionResult<IEnumerable<ToDoItem>>> GetToDoItems() {
             return await _context
                 .ToDoItems
                 .ToListAsync()
-                .ConfigureAwait(false)
-            ;
+                .ConfigureAwait(false);
         }
 
         // GET: api/ToDoItems/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<ToDoItem>> GetToDoItem(long id)
-        {
+        public async Task<ActionResult<ToDoItem>> GetToDoItem(long id) {
             var toDoItem = await _context.ToDoItems
                 .FindAsync(id)
-                .ConfigureAwait(false)
-            ;
+                .ConfigureAwait(false);
 
-            return toDoItem ?? (ActionResult<ToDoItem>)NotFound();
+            return toDoItem ?? (ActionResult<ToDoItem>) NotFound();
         }
 
         // PUT: api/ToDoItems/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutToDoItem(long id, ToDoItem toDoItem)
-        {
-            if (id != toDoItem.ID)
-            {
+        public async Task<IActionResult> PutToDoItem(long id, ToDoItem toDoItem) {
+            if (id != toDoItem.ID) {
                 return BadRequest();
             }
 
             _context.Entry(toDoItem).State = EntityState.Modified;
 
-            try
-            {
+            try {
                 await _context
                     .SaveChangesAsync()
-                    .ConfigureAwait(false)
-                ;
-            }
-            catch (DbUpdateConcurrencyException) when (!ToDoItemExists(id))
-            {
+                    .ConfigureAwait(false);
+            } catch (DbUpdateConcurrencyException) when(!ToDoItemExists(id)) {
                 return NotFound();
             }
 
@@ -75,13 +62,11 @@ namespace ToDoApi.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
         [HttpPost]
-        public async Task<ActionResult<ToDoItem>> PostToDoItem(ToDoItem toDoItem)
-        {
+        public async Task<ActionResult<ToDoItem>> PostToDoItem(ToDoItem toDoItem) {
             _context.ToDoItems.Add(toDoItem);
             await _context
                 .SaveChangesAsync()
-                .ConfigureAwait(false)
-            ;
+                .ConfigureAwait(false);
             const string actionName = nameof(GetToDoItem);
 
             return CreatedAtAction(actionName, new { id = toDoItem.ID }, toDoItem);
@@ -89,28 +74,23 @@ namespace ToDoApi.Controllers
 
         // DELETE: api/ToDoItems/5
         [HttpDelete("{id}")]
-        public async Task<ActionResult<ToDoItem>> DeleteToDoItem(long id)
-        {
+        public async Task<ActionResult<ToDoItem>> DeleteToDoItem(long id) {
             var toDoItem = await _context.ToDoItems
                 .FindAsync(id)
-                .ConfigureAwait(false)
-            ;
-            if (toDoItem == null)
-            {
+                .ConfigureAwait(false);
+            if (toDoItem == null) {
                 return NotFound();
             }
 
             _context.ToDoItems.Remove(toDoItem);
             await _context
                 .SaveChangesAsync()
-                .ConfigureAwait(false)
-            ;
+                .ConfigureAwait(false);
 
             return toDoItem;
         }
 
-        private bool ToDoItemExists(long id)
-        {
+        private bool ToDoItemExists(long id) {
             return _context.ToDoItems
                 .Any(e =>
                     e.ID == id
