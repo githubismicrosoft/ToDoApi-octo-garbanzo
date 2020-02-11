182ef05abd235421974f3469c537264f7b57d469
ml <30815731+slimlime@users.noreply.github.com>
2020-02-11 13:51:26 +1000

    Random autogen extension

--- Models/ToDoItem.cs
+++ Models/ToDoItem.cs
@@ -1,10 +1,14 @@
-namespace ToDoApi.Models
-{
+namespace ToDoApi.Models {
     public class ToDoItem {
         public long ID { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
-        
 
+        public ToDoItem(long ID, string name, bool isComplete)
+        {
+            this.ID = ID;
+            this.Name = name;
+            this.IsComplete = isComplete;
+        }
     }
 }
