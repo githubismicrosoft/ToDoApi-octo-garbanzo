4312f14d199c4cc9601f05759d7c9efdc14fd848
ml <30815731+slimlime@users.noreply.github.com>
2020-02-11 17:15:07 +1000

    DbContext ToDoList

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,7 @@ namespace ToDoApi {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             services.AddDbContext<ToDoContext>(opt =>
-                opt.UseInMemoryDatabase("TodoList")
+                opt.UseInMemoryDatabase("ToDoList")
             );
 
             services.AddControllers();
