e3b180b9e68cbb19691dbfca4f62e04df2c726e2
ml <30815731+slimlime@users.noreply.github.com>
2020-02-11 13:36:22 +1000

    Models

---
+++ Models/ToDoItem.cs
@@ -0,0 +1,10 @@
+namespace ToDoApi.Models
+{
+    public class ToDoItem {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+        
+
+    }
+}
